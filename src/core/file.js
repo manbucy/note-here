cae132a0efec8d79849cae08aae5f6669d06d7b0
manbucy <1412039603@qq.com>
2020-03-20 18:21:36 +0800

    更新-优化文件模块update

---
+++ src/core/file.js
@@ -0,0 +1,89 @@
+let fs = require('fs');
+let path = require('path');
+let dateFormat = require('dateformat');
+let template = require('art-template');
+let mime = require('mime');
+
+function get404Page() {
+    return '<!doctype html><title>404 Not Found</title><h1 style="text-align: center">404 Not Found</h1>';
+}
+
+function returnDir(res, stats, relativePath) {
+    let filePath = path.resolve(argv.dir, relativePath.substr(1));
+    fs.readdir(filePath, (err, files) => {
+        if (err) {
+            return404(res);
+        } else {
+            let dirArr = [];
+            let fileArr = [];
+            files.forEach(file => {
+                let stats = fs.statSync(path.resolve(filePath, file));
+                if (stats.isDirectory()) {
+                    dirArr.push({
+                        src: path.resolve(filePath, file).replace(argv.dir, ''),
+                        fileName: file,
+                        time: dateFormat(stats.mtime, 'yyyy-mm-dd HH:MM:ss')
+                    })
+                } else {
+                    fileArr.push({
+                        src: path.resolve(filePath, file).replace(argv.dir, ''),
+                        fileName: file,
+                        time: dateFormat(stats.birthtime, 'yyyy-mm-dd HH:MM:ss')
+                    })
+                }
+            });
+            html = template(path.resolve(__dirname, './template/index.html'), {
+                title: 'ManBu',
+                showTopDir: relativePath != '/',
+                topDir: getTopDir(relativePath),
+                dirs: dirArr,
+                files: fileArr
+            });
+            res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+            res.write(html);
+            res.end();
+        }
+    })
+}
+
+function returnFile(res, data, filePath) {
+    if (mime.getType(filePath) === mime.getType('md')) {
+        res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+        let html = template(path.resolve(__dirname, './template/article.html'), {
+            article: {
+                title: path.basename(filePath, ".md"),
+                articleTitle: path.basename(filePath, ".md"),
+                content: marked(data.toString())
+            }
+        });
+        res.write(html)
+    } else {
+        res.writeHead(200, {'Content-Type': mime.getType(filePath) + ';' + 'charset=UTF-8'});
+        res.write(data);
+    }
+    res.end();
+}
+
+function return404(res) {
+    res.writeHead(404, {'Content-Type': 'text/html;charset=UTF-8'});
+    res.write(get404Page());
+    res.end();
+}
+
+function getTopDir(path) {
+    if (path.endsWith('/')) {
+        path = path.substring(0, path.length - 1);
+    }
+    if (!path.startsWith('/')) {
+        path = '/' + path;
+    }
+    let pathArr = path.split('/');
+    pathArr[pathArr.length - 1] = '';
+    return pathArr.join('/')
+}
+
+module.exports = {
+    returnDir,
+    returnFile,
+    return404,
+}
